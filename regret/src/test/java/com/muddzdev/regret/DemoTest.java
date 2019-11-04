fc7768f6091f16ad46a8717a93ba919e795b5db2
Muddz <muddzdk@gmail.com>
2019-11-04 09:13:08 +0100

    refactoring

--- regret/src/test/java/com/muddzdev/regret/DemoTest.java
+++ regret/src/test/java/com/muddzdev/regret/DemoTest.java
@@ -6,14 +6,11 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class UseCaseTest {
-
-    //TODO better naming or include comment about this test
+public class DemoTest {
 
     private UndoRedoManager undoRedoManager;
     private static final String TEXT_COLOR_RED = "TEXT_COLOR_RED";
     private static final String TEXT_COLOR_GREEN = "TEXT_COLOR_GREEN";
-    private static final String TEXT_COLOR_BLUE = "TEXT_COLOR_BLUE";
     private static final String BACKGROUND_COLOR_WHITE = "BACKGROUND_COLOR_WHITE";
     private static final String BACKGROUND_COLOR_BLACK = "BACKGROUND_COLOR_BLACK";
 
@@ -22,7 +19,6 @@ public class UseCaseTest {
     public void setUp() {
         undoRedoManager = new UndoRedoManager();
         undoRedoManager.add(TEXT_COLOR_RED, Color.RED);
-        undoRedoManager.add(TEXT_COLOR_BLUE, Color.BLUE);
         undoRedoManager.add(TEXT_COLOR_GREEN, Color.GREEN);
         undoRedoManager.add(BACKGROUND_COLOR_WHITE, Color.WHITE);
         undoRedoManager.add(BACKGROUND_COLOR_BLACK, Color.BLACK);
@@ -32,10 +28,8 @@ public class UseCaseTest {
     public void testUndoRedo() {
         Assert.assertEquals(Color.WHITE, undoRedoManager.undo().getValue());
         Assert.assertEquals(Color.GREEN, undoRedoManager.undo().getValue());
-        Assert.assertEquals(Color.BLUE, undoRedoManager.undo().getValue());
         Assert.assertEquals(Color.RED, undoRedoManager.undo().getValue());
 
-        Assert.assertEquals(Color.BLUE, undoRedoManager.redo().getValue());
         Assert.assertEquals(Color.GREEN, undoRedoManager.redo().getValue());
         Assert.assertEquals(Color.WHITE, undoRedoManager.redo().getValue());
         Assert.assertEquals(Color.BLACK, undoRedoManager.redo().getValue());
