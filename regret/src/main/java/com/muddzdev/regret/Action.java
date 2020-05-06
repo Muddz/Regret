3079d58f2b05550b17f4555782f690232b337482
vahab <a1992b@yahoo.com>
2020-05-06 18:01:30 +0430

    add view to Action class--that which view in Action

--- regret/src/main/java/com/muddzdev/regret/Action.java
+++ regret/src/main/java/com/muddzdev/regret/Action.java
@@ -1,12 +1,20 @@
 package com.muddzdev.regret;
 
+import android.view.View;
+
 class Action {
 
     String key;
     Object value;
-
+    View view;
     Action(String key, Object value) {
         this.key = key;
         this.value = value;
+    }
+
+    Action(String key, Object value,View view) {
+        this.key = key;
+        this.value = value;
+        this.view=view;
     }
 }
